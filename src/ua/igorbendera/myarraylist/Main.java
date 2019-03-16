a7b7aadeb4d489585b4163db003484b1ba3bedea
Igor <ihor183@gmail.com>
2019-03-16 22:40:37 +0200

    Task 12(3)

--- src/ua/igorbendera/myarraylist/Main.java
+++ src/ua/igorbendera/myarraylist/Main.java
@@ -1,5 +1,28 @@
 package ua.igorbendera.myarraylist;
 
 public class Main {
+    public static void main(String[] args) {
+        MyArrayList myArrayList = new MyArrayList();
 
+        myArrayList.add(5);
+        myArrayList.add(15);
+        myArrayList.add(25);
+        myArrayList.add(35);
+        myArrayList.add(45);
+        myArrayList.add(55);
+        myArrayList.add(65);
+        myArrayList.add(75);
+        myArrayList.add(85);
+        myArrayList.add(95);
+
+        myArrayList.show();
+
+        myArrayList.remove(0);
+        myArrayList.remove(1);
+        myArrayList.remove(2);
+        myArrayList.remove(3);
+        myArrayList.remove(4);
+
+        myArrayList.show();
+    }
 }
--- src/ua/igorbendera/myarraylist/MyArrayList.java
+++ src/ua/igorbendera/myarraylist/MyArrayList.java
@@ -1,4 +1,59 @@
 package ua.igorbendera.myarraylist;
 
+import java.util.Arrays;
+
 public class MyArrayList {
+    private int[] array;
+    private int size;
+
+    public MyArrayList() {
+
+    }
+
+    public MyArrayList(int size) {
+        this.array = new int[size];
+    }
+
+    public boolean add(int value) {
+        increaseArray();
+        array[array.length - 1] = value;
+
+        return true;
+    }
+
+    public void increaseArray() {
+        if(size != 0) {
+            int[] tempArray;
+            tempArray = array;
+
+            array = new int[++size];
+
+            for(int i = 0; i < array.length - 1; i++) {
+                array[i] = tempArray[i];
+            }
+        } else {
+            array = new int[++size];
+        }
+    }
+
+    public void remove(int index) {
+        if(index >= 0 && index < size) {
+            int[] tempArray = new int[--size];
+
+            for(int i = 0; i < size; i++) {
+                if(i < index) {
+                    tempArray[i] = array[i];
+                } else {
+                    tempArray[i] = array[i + 1];
+                }
+            }
+
+            array = new int[size];
+            array = tempArray;
+        }
+    }
+
+    public void show() {
+        System.out.println(Arrays.toString(array));
+    }
 }
